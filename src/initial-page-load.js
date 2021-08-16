14b68bac455a921797e013d0756d90796432bc38
Chamara-Wijepala <mr.worf777@gmail.com>
2021-08-16 15:01:02 +0530

    Add function to create nav and append buttons to nav

--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -10,6 +10,31 @@ function createHeader() {
     
     content.appendChild(header);
     header.appendChild(cafeName);
+
+    return header;
+};
+
+function createNav(header) {
+    const nav = document.createElement('nav');
+    nav.id = 'nav';
+
+    const homeButton = document.createElement('button');
+    homeButton.classList.add('nav-button');
+    homeButton.textContent = 'Home';
+
+    const menuButton = document.createElement('button');
+    menuButton.classList.add('nav-button');
+    menuButton.textContent = 'Menu';
+
+    const contactButton = document.createElement('button');
+    contactButton.classList.add('Contact');
+    contactButton.textContent = 'Contact';
+
+    nav.appendChild(homeButton);
+    nav.appendChild(menuButton);
+    nav.appendChild(contactButton);
+
+    header.appendChild(nav);
 };
 
 function createMain() {
@@ -28,6 +53,7 @@ function createFooter() {
 
 export default function pageLoad() {
     createHeader();
+    createNav(header);
     createMain();
     createFooter();
 };
