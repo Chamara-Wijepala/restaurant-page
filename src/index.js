5083548903ff4b736da1e73222188587669928d0
Chamara-Wijepala <mr.worf777@gmail.com>
2021-08-19 16:42:23 +0530

    Add functions to implement tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import pageLoad from './initial-page-load';
 import loadHome from './home';
+import loadMenu from './menu';
+import loadContact from './contact';
 
 pageLoad();
 loadHome();
 
-function clearMain() {
-    const main = document.getElementById('main');
-    main.textContent = '';
-};
-
 const buttons = document.querySelectorAll('.nav-button');
 buttons.forEach(button => button.addEventListener('click', clearMain));
+
+function loadTab(buttonText) {
+    switch(buttonText) {
+        case 'Home':
+            loadHome();
+            break;
+        case 'Menu':
+            loadMenu();
+            break;
+        case 'Contact':
+            loadContact();
+            break;
+    };
+};
+
+function clearMain(event) {
+    const main = document.getElementById('main');
+    main.textContent = '';
+
+    const buttonText = event.target.innerText;
+
+    loadTab(buttonText);
+};
