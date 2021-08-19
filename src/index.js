9acf1fc0c3f2d8e071fce7c7ebfc19a837b3b49d
Chamara-Wijepala <mr.worf777@gmail.com>
2021-08-19 15:17:35 +0530

    Add function to clear elements from main

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import loadHome from './home';
 
 pageLoad();
 loadHome();
+
+function clearMain() {
+    const main = document.getElementById('main');
+    main.textContent = '';
+};
+
+const buttons = document.querySelectorAll('.nav-button');
+buttons.forEach(button => button.addEventListener('click', clearMain));
--- src/initial-page-load.js
+++ src/initial-page-load.js
@@ -38,7 +38,7 @@ function createMain() {
     
     content.appendChild(main);
 
-    return main
+    return main;
 };
 
 function createFooter() {
